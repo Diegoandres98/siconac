9d0bed071882cbee4f3c0284b7616a5efa2e8a3b
Diego Estrada <daestrada1998@gmail.com>
2021-03-07 18:19:26 -0500

    Ordenando Funciones

---
+++ Funciones/funciones.js
@@ -0,0 +1,66 @@
+
+        $(document).ready(function () {
+            $('#form').submit(function (e) {
+                e.preventDefault();
+                //captura todos los valores que tiene el formulario es decir todos los input que esten en ese formulario...
+                var datos=$(this).serialize();
+
+                $.ajax({
+                    type:"POST",
+                    url:"../Modelo/login.php",
+                    data:datos,
+                    success:function(data){
+                      if(data==1){
+                          Swal.fire({
+                              position: 'top-end',
+                              icon: 'success',
+                              title: 'Your work has been saved',
+                              showConfirmButton: false,
+                              timer: 1500
+                            })
+                            // window.location="../Vista/panel.php";
+                            location.href ="../Vista/panel.php";
+                        // setTimeout(window.location="../Vista/panel.php",3000);
+                      }
+                      else
+                      {
+                        Swal.fire({
+                        icon: 'error',
+                        title: 'Datos Incorrectos...',
+                        text: 'Para poder ingresar rectifica los datos suministrados',
+                        footer: '<a href>Si olvidaste la contraseña contacta con el administrador</a>'
+                        })
+                        // location.href ="../Vista/panel.php";
+                      }
+                        //imprimo el resultado en el div mensaje que procesa ajax
+                        $("#mensaje").html(data);
+                    }
+                });
+            });
+        });
+
+
+
+
+
+  function alertass()
+  {
+    Swal.fire({
+  icon: 'error',
+  title: 'Oops...',
+  text: 'Para poder registrarte debes comunicarte con el administrador',
+  footer: '<a href>Contactar Con Administrador</a>'
+  })
+  }
+  function alertasss()
+  {
+    Swal.fire({
+  title: 'NO puede ser',
+  text: 'la pagina dice que le dio amciedad que se te olvide la clave',
+  imageUrl: 'https://i.pinimg.com/564x/35/ec/cb/35eccb66592bcb023bd635c98327941e.jpg',
+  imageWidth: 400,
+  imageHeight: 200,
+  imageAlt: 'Custom image',
+})
+  }
+
--- Vista/login_v.php
+++ Vista/login_v.php
@@ -77,74 +77,7 @@
 <!-- /.login-box -->
 
 
-<script type="text/javascript">
-        $(document).ready(function () {
-            $('#form').submit(function (e) {
-                e.preventDefault();
-                //captura todos los valores que tiene el formulario es decir todos los input que esten en ese formulario...
-                var datos=$(this).serialize();
-
-                $.ajax({
-                    type:"POST",
-                    url:"../Modelo/login.php",
-                    data:datos,
-
-                    success:function(data){
-                      console.log(data);
-                      if(data==1){
-                       
-                        Swal.fire({
-                          position: 'top-end',
-                          icon: 'success',
-                          title: 'Your work has been saved',
-                          showConfirmButton: false,
-                          timer: 1500
-                        })
-                        location.href ="../Vista/panel.php";
-                      }
-                      else
-                      {
-                        Swal.fire({
-                        icon: 'error',
-                        title: 'Datos Incorrectos...',
-                        text: 'Para poder ingresar rectifica los datos suministrados',
-                        footer: '<a href>Si olvidaste la contraseña contacta con el administrador</a>'
-                        })
-                        // location.href ="../Vista/panel.php";
-                      }
-                        //imprimo el resultado en el div mensaje que procesa ajax
-                        $("#mensaje").html(data);
-                    }
-                });
-            });
-        });
-    </script>
-
-
-
-<script type="text/javascript">
-  function alertass()
-  {
-    Swal.fire({
-  icon: 'error',
-  title: 'Oops...',
-  text: 'Para poder registrarte debes comunicarte con el administrador',
-  footer: '<a href>Contactar Con Administrador</a>'
-  })
-  }
-  function alertasss()
-  {
-    Swal.fire({
-  title: 'NO puede ser',
-  text: 'la pagina dice que le dio amciedad que se te olvide la clave',
-  imageUrl: 'https://i.pinimg.com/564x/35/ec/cb/35eccb66592bcb023bd635c98327941e.jpg',
-  imageWidth: 400,
-  imageHeight: 200,
-  imageAlt: 'Custom image',
-})
-  }
-
-</script>
+<script src="../Funciones/funciones.js"></script>
 <!-- jQuery -->
 <script src="../plugins/jquery/jquery.min.js"></script>
 <script src="//cdn.jsdelivr.net/npm/sweetalert2@10"></script>
